1f8b7c5dd6874d4e0fbb7bf0a05a063706fee6fd
Putra Yehuda <putra.yehuda@ti.ukdw.ac.id>
2022-05-11 18:39:50 +0700

    Commit UG10A-LoginRegister

---
+++ UG10A-71200658/src/main/java/com/ugException/validate/Main.java
@@ -0,0 +1,25 @@
+package com.ugException.validate;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main
+{
+    public static void main( String[] args ) {
+        String inputUsername;
+        String inputEmail;
+        String inputPassword;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Selanat datang di Toko NamaKamu");
+        System.out.println("Silahkan daftarkan diri kamu untuk dapat mengakses menu di Toko NamaKamu");
+
+        System.out.print("Username : ");
+        inputUsername = scanner.nextLine();
+
+    }
+}
